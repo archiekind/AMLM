f9d57b6936271d6e43a14488e4fc6b55f89f0559
archiekind <archiekind1@gmail.com>
2023-08-29 14:47:29 +0100

    Added GQA and SwiGLU options. Output now written to CSV files.

--- model.py
+++ model.py
@@ -11,6 +11,9 @@ d_model = config["d_model"]
 batch_size = config["batch_size"]
 vocab_size = config["vocab_size"]
 block_length = config["block_length"]
+activation = config["activation"]
+attention = config["attention"]
+groups = config["groups"]
 device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
 
 #model
@@ -18,7 +21,6 @@ class MultiHeadAttention(nn.Module):
     def __init__(self):
         super().__init__()
         self.proj = nn.Linear(d_model, 3*d_model, bias=False)
-        self.w_out = nn.Linear(d_model, d_model, bias=False)
         self.register_buffer("mask", torch.tril(torch.ones(block_length, block_length)).view(1,1,block_length,block_length))
 
     def forward(self, x): # (b, t, c)
@@ -28,20 +30,44 @@ class MultiHeadAttention(nn.Module):
         k = k.view(b, t, heads, int(c/heads)).transpose(1,2) # (b, h, t, c/h)
         v = v.view(b, t, heads, int(c/heads)).transpose(1,2)
 
-        dk = k.size(dim=-1)
-        weights = (q @ k.transpose(-2,-1)) * (dk**-0.5) # (b, h, t, t)
+        weights = (q @ k.transpose(-2,-1)) * (k.size(dim=-1)**-0.5) # (b, h, t, t)
         weights = weights.masked_fill(self.mask[:,:,:t,:t] == 0, float('-inf'))
         weights = F.softmax(weights, dim=-1)
         out = weights @ v # (b, h, t, c/h)
         out = out.transpose(1, 2).contiguous().view(b,t,c) # (b, t, c)
-        #out = self.w_out(out)
         return out
-        
+    
+class GroupedQueryAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.proj_q = nn.Linear(d_model, d_model, bias=False)
+        self.proj_k = nn.Linear(d_model, int(d_model * groups / heads), bias=False)
+        self.proj_v = nn.Linear(d_model, int(d_model * groups / heads), bias=False)
+        self.register_buffer("mask", torch.tril(torch.ones(block_length, block_length)).view(1,1,block_length,block_length))
+
+
+    def forward(self, x):
+        b, t, c = x.size()
+        q = self.proj_q(x).view(b, t, heads, int(c/heads)).transpose(1,2) #(b, h, t, c/h)
+        k = self.proj_k(x).view(b, t, groups, int(c/heads)).transpose(1,2) #(b, g, t, c/h)
+        v = self.proj_v(x).view(b, t, groups, int(c/heads)).transpose(1,2) #(b, g, t, c/h)
+
+        h2g = heads // groups
+        weights = torch.cat([q[:,h2g * i:h2g * (i+1),:,:] @ k[:,i,:,:].transpose(-2,-1).view(b,1,c//heads,t) for i in range(k.size(dim=1))], dim=1)
+        weights = weights * (k.size(dim=-1)**-0.5) #(b, h, t, t)
+        weights = weights.masked_fill(self.mask[:,:,:t,:t] == 0, float('-inf'))
+        weights = F.softmax(weights, dim=-1)
+        out = torch.cat([weights[:,h2g *i:h2g*(i+1),:,:] @ v[:,i,:,:].view(b,1,t,c//heads) for i in range(v.size(dim=1))], dim=1) # (b, h, t, c/h)
+        out = out.transpose(1, 2).contiguous().view(b,t,c) #(b, t, c)
+
+        return out
+
+
 class FeedForward(nn.Module):
     def __init__(self):
         super().__init__()
-        self.ffw1 = nn.Linear(d_model, 4*d_model)
-        self.ffw2 = nn.Linear(4*d_model, d_model)
+        self.ffw1 = nn.Linear(d_model, 4*d_model, bias=False)
+        self.ffw2 = nn.Linear(4*d_model, d_model, bias=False)
         self.relu = nn.ReLU()
 
     def forward(self, x): # (b, t, c)
@@ -50,11 +76,23 @@ class FeedForward(nn.Module):
         out = self.ffw2(out)
         return out
     
+class FeedForwardSwiGLU(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.W1 = nn.Linear(d_model, int(4*d_model*(2/3)), bias=False) #scaling the hidden units to keep computation about constant
+        self.V = nn.Linear(d_model, int(4*d_model*(2/3)), bias=False)
+        self.W2 = nn.Linear(int(4*d_model*2/3), d_model, bias=False)
+    
+    def forward(self, x):
+        out = F.silu(self.W1(x)) * self.V(x)
+        out = self.W2(out)
+        return out
+
 class TransformerBlock(nn.Module):
     def __init__(self):
         super().__init__()
-        self.attn = MultiHeadAttention()
-        self.ffw = FeedForward()
+        self.attn = MultiHeadAttention() if config["attention"] == "MHA" else GroupedQueryAttention()
+        self.ffw = FeedForward() if config["activation"] == "ReLU" else FeedForwardSwiGLU()
         self.ln1 = nn.LayerNorm(d_model)
         self.ln2 = nn.LayerNorm(d_model)
 
@@ -67,10 +105,11 @@ class TransformerBlock(nn.Module):
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
+        print(f"Using {config['attention']} and {config['activation']}", '\n')
         self.tok_emb = nn.Embedding(vocab_size, d_model) 
         self.pos_emb = nn.Embedding(block_length, d_model)
         self.blocks = nn.ModuleList(TransformerBlock() for _ in range(layers))
-        self.ffw = nn.Linear(d_model, vocab_size)
+        self.ffw = nn.Linear(d_model, vocab_size, bias=False)
 
 
     def forward(self, x, y=None):
--- trainer.py
+++ trainer.py
@@ -6,6 +6,8 @@ import torch
 import matplotlib.pyplot as plt
 import random
 import os
+import time
+import csv
 
 # hyperparameters
 config = json.load(open("./config.json"))
@@ -17,8 +19,9 @@ lr = config["learning_rate"]
 device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
 
 # dataset config
+print('\n')
 print("loading dataset ...", '\n')
-dataset = load_dataset("cnn_dailymail", "3.0.0", split="train[:50%]").shuffle()
+dataset = load_dataset("cnn_dailymail", "3.0.0", split="train").shuffle()
 dataset_val = load_dataset("cnn_dailymail", "3.0.0", split="validation").shuffle()
 tokeniser = Tokenizer.from_file("./tokeniser.json")
 
@@ -28,6 +31,7 @@ def decode(x):
         out[i] = out[i].replace("Ġ", " ")
     print("".join(out))
 
+print('\n')
 print("formatting dataset ...", '\n')
 
 def get_input_ids(data):
@@ -72,14 +76,36 @@ def estimate_val_loss():
     transformer.train()
     return sum(loss_vali)/len(loss_vali)
     
+def estimate_train_loss():
+    transformer.eval()
+    loss_traini = []
+    for _ in range(val_steps):
+        rndm1 = random.randint(0, (len_dataset-batch_size)//batch_size)
+        list = dataset[batch_size*rndm1 : batch_size*(rndm1+1)]
+        for j in range(len(list)):
+            rndm2 = random.randint(0, len(list[j]) - block_length - 1)
+            list[j] = list[j][rndm2: rndm2 + block_length + 1]
+        x = torch.tensor(list)[:,:-1].to(device)
+        y = torch.tensor(list)[:,1:].to(device)
+        out, loss = transformer(x, y)
+        loss_traini.append(loss.item())
+        transformer.train()
+        return sum(loss_traini)/len(loss_traini)
 
 
 #trainer
+f_loss = open("./csvs/loss.csv", 'w')
+writer_loss = csv.writer(f_loss)
+f_loss_step = open("./csvs/loss_step.csv", 'w')
+writer_loss_step = csv.writer(f_loss_step)
+
 train_loss = []
 val_loss = []
-
+losses = []
 lossi = []
+
 print("beginning training ...", '\n')
+start_time = time.time()
 for i in range(training_steps):
     optimiser.zero_grad(set_to_none=True)
     rndm1 = random.randint(0, (len_dataset-batch_size)//batch_size)
@@ -94,21 +120,29 @@ for i in range(training_steps):
     optimiser.step()
 
     lossi.append(loss.item())
-
-    if i % 500 == 0 and i != 0:
-        loss_val = estimate_val_loss()
-        loss_train = sum(lossi)/len(lossi)
+    if len(lossi) == 10:
+        losses.append(sum(lossi)/10)
+        writer_loss.writerow([(sum(lossi)/10)])
         lossi = []
+
+    if i % 1000 == 0 and i != 0:
+        loss_val = estimate_val_loss()
+        loss_train = estimate_train_loss()
         train_loss.append(loss_train)
         val_loss.append(loss_val)
-        print(f"step: {i} --- training loss: {loss_train} --- validation loss: {loss_val}")
+        writer_loss_step.writerow([loss_train, loss_val])
+        print(f"step: {i} --- training loss: {loss_train} --- validation loss: {loss_val} --- time: {time.time() - start_time}", '\n')
 
 torch.save(transformer.state_dict(), "./model")
-
+f_loss.close()
+f_loss_step.close()
 
 #generate sample
 generation = transformer.generate(torch.tensor([32], dtype=torch.long).to(device), 150)
 decode(generation)
+
+plt.plot(losses)
+plt.show()
 
 plt.plot(train_loss)
 plt.show()
